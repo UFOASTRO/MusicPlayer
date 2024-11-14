ba48eb3bf56ccfdcbfbaf570b07b9c283d076eee
Astro <nifemibello13@gmail.com>
2024-11-14 16:57:20 +0100

    add first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let progress = document.querySelector('.progress');
+let song = document.querySelector('.song');
+let control = document.getElementById('controlIcon');
+let picture = document.querySelector('.picture');
+song.onloadedmetadata = function(){
+   progress.max = song.duration;
+   progress.value = song.currentTime;  
+}
+
+function playPause() {
+    if (control.classList.contains('fa-pause')) {
+        song.pause();
+        control.classList.remove('fa-pause');
+        control.classList.add('fa-play');
+        picture.classList.remove('playing');
+        picture.classList.add('asake') // Pause animation
+    } else {
+        song.play();
+        picture.classList.add('playing'); // Resume animation
+        control.classList.add('fa-pause');
+        control.classList.remove('fa-play');
+    }
+}
+
+song.ontimeupdate = function() {
+    progress.value = song.currentTime;
+};
+
+control.addEventListener('click', playPause);
